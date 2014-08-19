a0d6c9716760af4a6e22f6e3e3fec5e99bdd2b01
Ramith Jayatilleka <ramith.jayatilleka@gmail.com>
2014-08-19 10:47:47 -0500

    Cleaned up sieve.hs

--- sieve.hs
+++ sieve.hs
@@ -1,13 +1,14 @@
 import Data.List (genericReplicate)
 
-sieveEvery :: Integer -> [Integer] -> [Integer]
-sieveEvery n = zipWith ($) sieveToZero
-    where sieveToZero = tail (cycle (const 0 : genericReplicate (n - 1) id))
-
+-- Given a list of consecutive integers, with leading composites set to 0
+-- Will set to zero all composites
 sieve :: [Integer] -> [Integer]
+sieve [] = []
 sieve (0:xs) = 0 : sieve xs
 sieve (x:xs) = x : sieve (sieveEvery x xs)
-sieve [] = []
+  where sieveEvery n = zipWith (*) (markComposites n)
+        markComposites n = tail (cycle (0 : genericReplicate (n - 1) 1))
 
+-- Generates an infinite list of prime numbers
 primes :: [Integer]
 primes = filter (/=0) (sieve [2..])
